658f4191acbbc1e85f6076f0961e4c27060cf506
paulo <90739474+kurigohan1@users.noreply.github.com>
2024-01-24 21:57:44 -0300

    Create 1002.cpp

---
+++ 1002.cpp
@@ -0,0 +1,25 @@
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+int main(){
+	double pi;
+	pi=3.14159;
+	double raio;
+	cin>>raio;
+	double area;
+	area = pi * raio*raio;
+	cout<<fixed<<setprecision(4);
+	cout<<"A="<<area<<endl;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
